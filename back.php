aa834dca85dca8758b2fc136c1665337ab8c06c4
awanish_mishra <awanish.mishra@gingerwebs.co.in>
2023-05-27 10:42:26 +0530

    ajax code

---
+++ back.php
@@ -0,0 +1,66 @@
+<?php 
+//mysql -h13.232.43.166 --port=3310 -udevelopment -pAllenginger354 -A
+$conn = mysqli_connect('13.232.43.166:3310','development','Allenginger354','kotanew21');
+if (!$conn ) {
+   echo "Not connect databases";
+}
+
+$post = $_POST['name'];
+
+if ($post == 'view') {
+    $sql = mysqli_query($conn, "SELECT * FROM STUDENT LIMIT 10");
+    $student = array();
+    while($row=mysqli_fetch_assoc($sql)){
+        $student[] = $row;
+    }
+    echo json_encode( $student);
+
+}elseif($post == 'edit'){
+    
+    $id = $_POST['id'];
+    $sql = mysqli_query($conn, "SELECT * FROM STUDENT WHERE STUDENT_ID=$id");
+   $row=mysqli_fetch_assoc($sql);
+    echo json_encode($row);
+    
+
+}elseif($post == 'update'){
+
+    $id = $_POST['student'];
+    $countId = count($id);
+
+    $update = "update student set ";
+    foreach ($id as $key => $value) {
+
+        $countId --;
+
+        $update .= " $key = '$value'";
+
+        if($countId > 0){
+            if($countId == 1 ){
+                $update .= " where ";
+            }else{
+                $update .= ",";
+            }
+            
+           }
+       
+       
+    }
+
+    $sql = mysqli_query($conn, $update);
+    if ($sql) {
+        $arr = array("msg"=>"data updated");
+    } else {
+        $arr = array("msg"=>"data not updated");
+    }
+    echo json_encode($arr);
+
+}else{
+
+}
+
+
+
+
+
+?>
--- view.php
+++ view.php
@@ -1,12 +1,173 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ajax</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ajax</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 </head>
+
 <body>
-    
+
+    <div class="conatiner">
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        STUDENT_NAME : <input type="text" id="STUDENT_NAME" value="">
+                        <br><br>
+                        ENROLLMENT_NO : <input type="text" id="ENROLLMENT_NO" value="">
+                        <br>
+                        Male <input type="radio" name="gender" id="Male" value="Male">
+                        FeMale <input type="radio" name="gender" id="Female" value="Female">
+                        <br>
+                        DOB : <input type="date" id="DOB" value="">
+                        <input type="hidden" id="STUDENT_ID" value="">
+
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary" onclick="updateFunction()" data-bs-dismiss="modal">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container m-5" id="myTable">
+
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
+    <script>
+        $(document).ready(() => {
+            viewFunction();
+
+        });
+
+
+        function viewFunction() {
+
+
+            $.ajax({
+                type: 'post',
+                url: 'back.php',
+                data: {
+                    "name": "view"
+                },
+                success: function(result) {
+
+                    data = JSON.parse(result);
+                    table = ` <table class="table table-success table-striped">
+                        <tr class="table-primary">
+                        <td class="table-primary">STUDENT_ID</td>
+                        <td class="table-secondary">ENROLLMENT_NO</td>
+                        <td class="table-success">STUDENT_NAME</td>
+                        <td class="table-danger">GENDER</td>
+                        <td class="table-warning">DOB</td>
+                        <td class="table-warning">Action</td>
+
+                    </tr>`;
+                    for (let i = 0; i < data.length; i++) {
+
+                        table += `<tr class="table-primary">
+                        <td class="table-primary">${data[i].STUDENT_ID}.</td>
+                        <td class="table-secondary">${data[i].ENROLLMENT_NO}</td>
+                        <td class="table-success">${data[i].STUDENT_NAME}</td>
+                        <td class="table-danger">${data[i].GENDER}</td>
+                        <td class="table-warning">${data[i].DOB}</td>
+                        <td class="table-warning"> <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn btn-primary" onClick="editFunction(${data[i].STUDENT_ID})">Edit</button>  <button type="button" class="btn btn-danger" onClick="deleteFunction(${data[i].STUDENT_ID})">Delete</button> </td>
+                    </tr>`;
+
+
+                    }
+                    table += `</table>`;
+
+                    $('#myTable').html(table);
+                    return false;
+
+                }
+
+
+            })
+        }
+
+        function editFunction(id) {
+            $.ajax({
+                type: 'post',
+                url: 'back.php',
+                data: {
+                    "name": "edit",
+                    "id": id
+                },
+                success: function(result) {
+                    data = JSON.parse(result);
+                    $('#STUDENT_NAME').val(data.STUDENT_NAME);
+                    $('#ENROLLMENT_NO').val(data.ENROLLMENT_NO);
+                    if (data.GENDER == 'Male') {
+                        $('#Male').prop('checked', true);
+
+                    } else if ((data.GENDER == 'Female')) {
+                        $('#Female').prop('checked', true);
+                    }
+                    $('#DOB').val(data.DOB);
+                    $('#STUDENT_ID').val(data.STUDENT_ID);
+
+
+
+                }
+            });
+
+        }
+
+        function updateFunction() {
+            STUDENT_NAME = $('#STUDENT_NAME').val();
+            ENROLLMENT_NO = $('#ENROLLMENT_NO').val();
+            if ($('#Male').is(":checked")) {
+                gender = $('#Male').val();
+
+            } else if ($('#Female').is(":checked")) {
+                gender = $('#Female').val();
+            }
+            DOB = $('#DOB').val();
+            STUDENT_ID = $('#STUDENT_ID').val();
+
+            Studentdata = {
+                "STUDENT_NAME": STUDENT_NAME,
+                "ENROLLMENT_NO": ENROLLMENT_NO,
+                "gender": gender,
+                "DOB": DOB,
+                "STUDENT_ID": STUDENT_ID
+            }
+
+            $.ajax({
+                type: 'post',
+                url: 'back.php',
+                data: {
+                    "name": "update",
+                    "student": Studentdata
+                },
+                success: function(result) {
+                    data = JSON.parse(result);
+                    alert(data.msg);
+                    viewFunction();
+
+                }
+            });
+
+
+        }
+    </script>
 </body>
+MEHUL DARAK
+
 </html>
