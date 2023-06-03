1a35e7de8db60ac9ca49a0c3b7f67ee40c08479e
awanish_mishra <awanish.mishra@gingerwebs.co.in>
2023-06-03 13:03:05 +0530

    array crud

---
+++ arrayCrud/view.php
@@ -0,0 +1,51 @@
+<?php 
+ session_start();
+ $firstArr = [1,12,3,3,4,4,5,32]; /// indexing or onedymation array
+
+ // echo count($firstArr);
+ // die;
+
+ // $firstArr[7] = 10; #insert array value
+ // $firstArr[3] = 5; #udate array value
+ // unset($firstArr[5]); //delete array value by key 
+// $count = count($firstArr);
+
+// for ($i = 0; $i <count($firstArr); $i++) {
+	
+// 	echo $firstArr[$i];
+
+
+// }
+// $_SESSION[0] = "sdf";
+//      echo "<pre> hii awani u r ok ⭐";
+//   print_r($_SESSION);
+  
+//      echo "</pre>";
+//      die();
+     
+  
+//      die();
+
+ $table = array();
+ for ($i = 1; $i <= 10; $i++) {
+
+ 	$table[$i] = 2*$i;
+ 	
+ }
+        echo "<pre> hii awani u r ok ⭐";
+     print_r($table);
+     
+        echo "</pre>";
+        die();
+        
+    
+
+
+
+
+
+
+
+
+
+ ?>
